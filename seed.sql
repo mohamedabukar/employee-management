8ad36d66a65838505a3a446cc95994bd95b0672c
Mohamed <mohamed.h.abukar@gmail.com>
2021-08-09 19:35:12 -0500

    seed data added.

--- seed.sql
+++ seed.sql
@@ -10,8 +10,8 @@ INSERT INTO employee_role
 (title, salary, department_id)
 VALUES
 ('Engineer', 50000, 1),
-('IT Manager', 60000, 1)
-('Accountant', 70000, 2)
+('IT Manager', 60000, 1),
+('Accountant', 70000, 2);
 
 INSERT INTO employee
 (first_name, last_name, role_id, manager_id)
