2e70cbbdbfb08e565852c91909d74272ebdd09b1
Mohamed <mohamed.h.abukar@gmail.com>
2021-08-09 19:34:49 -0500

    add employee option added.

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-const add = require("./lib/add");
-const remove = require("./lib/remove");
-const update = require("./lib/update")
+const table= require("console.table")
+
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -17,48 +16,117 @@ connection.connect((err) => {
 })
 const runsearch = () => {
     inquirer
-    .prompt({
-        name: "action",
-        type: "rawlist",
-        message: "What would you like to do?",
-        choices:[
-            "View all employees",
-            "View employees by department",
-            "View employees by manager",
-            "Add Employee",
-            "Remove Employee",
-            "Update employee role",
-            "Update employee manager"
-        ]
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View all employees",
+                "View departments",
+                "View roles",
+                "Add Employee",
+                "Remove Employee",
+                "Update employee role",
+                "Update employee manager"
+            ]
+        })
+        .then((answer) => {
+            switch (answer.action) {
+                case "View all employees":
+                    viewemployees();
+                    break;
+                case "View departments":
+                    viewDepartments();
+                    break;
+                case "View roles":
+                    viewRoles();
+                    break;
+                case "Add Employee":
+                    addEmployee();
+                    break;
+                case "Remove Employee":
+                    removeEmployee();
+                    break;
+                case "Update employee role":
+                    updateRoles();
+                    break;
+
+                case "Update employee manager":
+                    updateManager();
+                    break;
+                default:
+                    console.log(`Invalid action: ${answer.action}`);
+                    break;
+            }
+
+        })
+}
+
+const viewemployees = () => {
+    const query = "SELECT * FROM employee";
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        runsearch();
     })
-    .then((answer) => {
-        switch (answer.action) {
-            case "View all employees":
-                viewemployees();
-                break;
-            case "View employees by department":
-                viewByDepartment();
-                break;
-            case "View employees by manager":
-                viewByManager();
-                break;
-            case "Add Employee":
-                addEmployee();
-                break;
-            case "Remove Employee":
-                removeEmployee();
-                break;
-            case "Update employee role":
-                updateRoles();
-                break;
-            
-            case "Update employee manager":
-                updateManager();
-                break;
-            default:
-                console.log(`Invalid action: ${answer.action}`);
-                break;
-        }
-            
+}
+const viewDepartments = () => {
+    const query = "SELECT * FROM department";
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        runsearch();
     })
+}
+
+
+const viewRoles = () => {
+    const query = "SELECT title FROM employee_role";
+    connection.query(query, (err, res) => {
+        if (err) throw errr;
+        console.table(res);
+        roleTable.push(res);
+        console.log(roleTable);
+        runsearch();
+    })
+}
+const addEmployee = () => {
+    const query = "SELECT title FROM employee_role";
+    connection.query(query, (err, res) => {
+        if (err) throw errr;
+        console.table(res);
+        console.log(res, typeof res);
+
+        inquirer
+            .prompt([
+                {
+                    type: "input",
+                    message: "What is the employee's first name?",
+                    name: "firstName"
+                },
+                {
+                    type: "input",
+                    message: "What is the employees last name?",
+                    name: "lastName"
+                },
+                {
+                    type: "list",
+                    message: "What is the employees role?",
+                    name: "role",
+                    choices: res.map((role, index) => {
+                        let i = index + 1
+                        return i + ". " + role.title;
+                    })
+                }
+            ])
+            .then(answers => {
+                let roleId = answers.role.split(". ")[0]
+                const query = "INSERT INTO employee SET ?";
+                connection.query(query, { first_name: answers.firstName, last_name: answers.lastName, role_id: roleId }, (err, res) => {
+                    if (err) throw err;
+                    viewemployees();
+                })
+            })
+    })
+
 }
